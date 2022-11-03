69d79cb9f5718212d7a5309ffbc9761768d37a27
Facundo Lorenzo <facundoilorenzo@gmail.com>
2022-11-03 16:50:14 -0300

    filtros

--- src/components/cardGrid/CardGrid.jsx
+++ src/components/cardGrid/CardGrid.jsx
@@ -1,15 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Filter from "./filters/Filter";
 import Card from "../card/Card";
 import "./cardGrid.css";
 
 function CardGrid({ grid }) {
   return (
     <div className="container">
+      <Filter />
+      {/* <Link to={}> */}
       <div className="grid">
         {grid.map((data) => (
           <Card key={data.id} data={data} />
         ))}
       </div>
+      {/* </Link> */}
     </div>
   );
 }
